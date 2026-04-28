8945641248720629728a4c97f9439112554d1540
Bence Mihucza <mihben@gmail.com>
2026-04-28 18:39:37 +0200

    chore: Added sample projects

---
+++ src/Sample.Test.Unit/SampleTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Sample.Test.Unit
+{
+    public class SampleTest
+    {
+        [Fact(DisplayName = "Pass Test")]
+        public void Pass_Test()
+        {
+            Assert.True(true);
+        }
+
+        [Fact(DisplayName = "Fail Test", Skip = "Execute only if testing the pipeline termination")]
+        public void Fail_Test()
+        {
+            Assert.Fail("This is a failed test :(");
+        }
+    }
+}
